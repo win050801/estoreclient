d25c71f4728d2fda1d272c532173ab1ddc440b20
CaoThang <tcao2315@gmail.com>
2022-11-21 03:13:48 +0700

    finish header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Home from "./pages/Home";
+import ModelLogin from "./components/Model/ModelLogin";
+import Login from "./pages/login/Login";
+import AppProvider from "./context/AppProvider";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    return (
+      <Router>
+        <AppProvider>
+          <Routes>
+            <Route element={<Home />} path="/" />
+            <Route element={<Login />} path="/login" />
+          </Routes>
+          <ModelLogin />
+          
+        </AppProvider>
+      </Router>
+    );
+  }
+  
+  export default App;
